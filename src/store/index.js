92c4addb802046f70323bbe79d7f4d63cfc56f7a
Deyan <deyan-a@protonmail.com>
2020-02-16 15:00:25 +0200

    added camel-case to dependencies and added redux-devtools browser extension support to the store

--- src/store/index.js
+++ src/store/index.js
@@ -4,5 +4,9 @@ import promise from 'redux-promise-middleware';
 import rootReducer from './modules';
 
 const middleware = applyMiddleware(promise, thunk);
+const composeEnhancers =
+    (typeof window !== 'undefined' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
 
-export default createStore(rootReducer, compose(middleware));
+export default createStore(rootReducer, composeEnhancers(middleware));
