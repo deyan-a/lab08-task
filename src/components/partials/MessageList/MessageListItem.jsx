a770224171766eba0a0c839b3391d1754fd74a56
Deyan <deyan-a@protonmail.com>
2020-02-22 14:50:10 +0200

    fixed a bug with messages without a score, removed redundant .negative-score class.

--- src/components/partials/MessageList/MessageListItem.jsx
+++ src/components/partials/MessageList/MessageListItem.jsx
@@ -4,10 +4,10 @@ import classnames from 'classnames';
 import { getMessageById } from '../../../store/selectors';
 
 function MessageListItem({ message, className, isCollapsed }) {
-    const { text, question, subject, team, created_at: createdAt, score } = message;
+    const { text, question, subject, team, created_at: createdAt, score = 0 } = message;
 
     const containerClass = classnames('message-container', {[className]: isCollapsed})
-    const titleClass = classnames('msg-title', { 'positive-score': score >= 6, 'negative-score': score <= 5 });
+    const titleClass = classnames('msg-title', { 'positive-score': score >= 6 });
     const createdDate = new Date(createdAt).toLocaleDateString('en-GB', { day: 'numeric', month: 'short'});
 
     return (
